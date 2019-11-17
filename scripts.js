e93ad9b8646d868d68cad4ebaf02b7d0a38db57b
Hunteriapd2 <hunteripad2@gmail.com>
2019-11-17 21:23:58 +0400

    Added Event Functional

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ function showMusicMenu() {
 
 function closeTabs() {
   document.querySelector('.music-menu').style.transform = "translate(100%)";
+  document.querySelector('.event').style.transform = "scale(0, 0)";
   document.querySelector('#blackening').style.opacity = "0";
   document.querySelector('#blackening').style.transform = "translate(100%)";
 }
@@ -145,12 +146,162 @@ function playCurrentMusic() {
   if (event.currentTarget.hasAttribute("pause")) {
     currentMusic.play();
     event.target.src = "pause-music-button.png";
-    event.currentTarget.removeAttribute("pause")
+    event.currentTarget.removeAttribute("pause");
     event.target.title="Поставить на паузу";
   } else {
     currentMusic.pause();
     event.target.src = "play-music-button.png";
-    event.currentTarget.setAttribute("pause", "")
+    event.currentTarget.setAttribute("pause", "");
     event.target.title="Снять с паузы";
   }
+}
+
+
+
+
+let currentTurn = 1;
+
+const allData = {
+  scenarioFirst: [
+    {
+      name: "Historical",
+      turn: 1,
+      date: "Январь 1910г.",
+      map: "map.png",
+      events: [
+      ]
+    }
+  ],
+
+  scenarioSecond: [
+    {
+      name: "Historical",
+      turn: 1,
+      date: "Январь 1924г.",
+      map: "map.png",
+      events: [
+      ]
+    },
+  ],
+
+  scenarioThird: [
+    {
+      name: "Historical",
+      turn: 1,
+      date: "Январь 1934г.",
+      map: "map-1934-01-historical.png",
+      events: [
+        {
+          name: "Название события номер 2",
+          desc: "Описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание описание",
+          image: "scenario-second.jpg",
+          option1: "Первая опция",
+          type: "info",
+          icon: "red-circle.png",
+          positionX: "450px",
+          positionY: "550px",
+        },
+        {
+          name: "Название события номер 3",
+          desc: "Описание описание описание описание описание описание описание",
+          image: "scenario-second.jpg",
+          option1: "Первая опция",
+          type: "info",
+          icon: "red-circle.png",
+          positionX: "600px",
+          positionY: "600px",
+        },
+        {
+          name: "Название музыкального события",
+          desc: "Описание описание описание описание описание описание описание",
+          image: "scenario-second.jpg",
+          option1: "Не добавлять в список",
+          option2: "Добавить в список",
+          type: "music",
+          icon: "red-circle.png",
+          positionX: "380px",
+          positionY: "450px",
+          musicUnlockName: "Музыка 2",
+          musicUnlockSrc: "example2.ogg",
+        },
+      ]
+    },
+  ],
+}
+
+let scenario = allData[document.URL.slice(document.URL.indexOf("?") + 1)];
+
+document.addEventListener('DOMContentLoaded', choosePeriod);
+function choosePeriod() {
+  let choosenPeriod;
+
+  if (currentTurn === 1) {
+    choosenPeriod = scenario.find(period => period.turn === 1);
+  }
+
+  document.querySelector('.map__map').src = choosenPeriod.map;
+  document.querySelector('.turn-counter__date').innerHTML = choosenPeriod.date;
+
+  let eventList = choosenPeriod.events;
+  for (let i = 0; i < eventList.length; i += 1) {
+    let name = eventList[i].name;
+    let desc = eventList[i].desc;
+    let image = eventList[i].image;
+    let option1 = eventList[i].option1;
+    let option2 = eventList[i].option2;
+    let type = eventList[i].type;
+    let musicName = eventList[i].musicUnlockName;
+    let musicSrc = eventList[i].musicUnlockSrc;
+    let icon = eventList[i].icon;
+    let positionX = eventList[i].positionX;
+    let positionY = eventList[i].positionY;
+
+    document.querySelector('.map').insertAdjacentHTML('beforeend', `<button style="left: ${positionX}; top: ${positionY}" class="map__event-button" onclick="showEventWindow('${name}', '${desc}', '${image}', '${option1}', '${option2}', '${type}', '${musicName}', '${musicSrc}')"><img src="${icon}" class="map__event-icon"></button>`);
+  }
+}
+
+setInterval(() => {
+  let events = document.querySelectorAll('.map__event-button');
+  
+  for (let evnt of events) {
+    if (getComputedStyle(evnt).opacity === "1") {  
+      evnt.style.opacity = "0.2";
+    } else if (!evnt.hasAttribute("checked")) {
+      evnt.style.opacity = "1";
+    }
+  }
+}, 500);
+
+function showEventWindow(name, desc, image, option1, option2, type, musicName, musicSrc) {
+  event.currentTarget.setAttribute("checked", "");
+
+  document.querySelector('.event__name').innerHTML = name;
+  document.querySelector('.event__image').src = image;
+  document.querySelector('.event__desc').innerHTML = desc;
+  document.querySelector('.event__button-first').innerHTML = option1;
+  document.querySelector('.event__button-second').innerHTML = option2;
+  document.querySelector('.event').setAttribute("eventtype", `${type}`);
+  document.querySelector('.event').setAttribute("musicname", `${musicName}`);
+  document.querySelector('.event').setAttribute("musicsrc", `${musicSrc}`);
+
+  document.querySelector('.event').style.transform = "scale(1, 1)";
+  document.querySelector('#blackening').style.opacity = "0.8";
+  document.querySelector('#blackening').style.transform = "translate(0%)";
+
+  if (type === "music") {
+    document.querySelector('.event__button-second').style = "visibility: visible";
+    document.querySelector('.event__button-first').style = "border-top-left-radius: 0; border-top-right-radius: 0";
+  } else { 
+    document.querySelector('.event__button-second').style = "visibility: hidden";
+    document.querySelector('.event__button-first').style = "border-top-left-radius: 25px; border-top-right-radius: 25px";
+  }
+}
+
+function chooseEventOption() {
+  let musicName = event.target.parentNode.getAttribute("musicname");
+  let musicSrc = event.target.parentNode.getAttribute("musicsrc");
+  if (event.target.parentNode.getAttribute("eventtype") === "music" && event.target === document.querySelector('.event__button-second')) {
+    document.querySelector('.music-menu').insertAdjacentHTML('beforeend', `<li class="music-menu__item" onclick="chooseThisMusic()"><span class="music-menu__item-name">${musicName}</span><audio class="inactive-music" onended="playNextMusic()"><source src="${musicSrc}" type="audio/ogg"></audio><img src="forbid-music-unckecked.png" class="music-menu__item-forbid" onclick="forbidMusic()" title="Запретить воспроизведение"></li>`);
+  }
+  closeTabs();
 }
